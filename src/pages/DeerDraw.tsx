9a57aa3c1d0210f268e1710ee99b45248afc8838
carrellcody <carrellcody18@gmail.com>
2025-11-26 20:08:44 -0600

    Adjust padding in DeerDraw component

--- src/pages/DeerDraw.tsx
+++ src/pages/DeerDraw.tsx
@@ -17,7 +17,7 @@ export default function DeerDraw() {
     return null;
   }
   return (
-    <div className="container mx-auto pt-2 pb-4 h-[calc(100vh-8rem)]">
+    <div className="container mx-auto pt-2 pb-10 h-[calc(100vh-8rem)]">
       <div className="flex items-start gap-6 mb-2">
         <div className="flex-shrink-0">
           <h1 className="text-3xl font-bold mb-1">Deer Draw Statistics</h1>
