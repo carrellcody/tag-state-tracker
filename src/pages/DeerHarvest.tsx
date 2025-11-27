933686ce0335d6863f9f8ae847eec42f391ccf9a
carrellcody <carrellcody18@gmail.com>
2025-11-26 20:15:11 -0600

    Adjust padding in DeerHarvest component
    Updated padding in the DeerHarvest component.

--- src/pages/DeerHarvest.tsx
+++ src/pages/DeerHarvest.tsx
@@ -21,7 +21,7 @@ export default function DeerHarvest() {
   }
 
   return (
-    <div className="container mx-auto pt-2 pb-4 h-[calc(100vh-8rem)]">
+    <div className="container mx-auto pt-2 pb-10 h-[calc(100vh-8rem)]">
       <div className="flex items-start gap-6 mb-2">
         <div className="flex-shrink-0">
           <h1 className="text-3xl font-bold mb-1">Deer Harvest Statistics</h1>
