143945f71d1131cc2a4213f1062a63cf7d6b6254
carrellcody <carrellcody18@gmail.com>
2025-11-26 20:14:46 -0600

    Adjust padding in AntelopeHarvest component
    Increase bottom padding of the container div.

--- src/pages/AntelopeHarvest.tsx
+++ src/pages/AntelopeHarvest.tsx
@@ -3,7 +3,7 @@ import { AntelopeHarvestTable } from '@/components/tables/AntelopeHarvestTable';
 
 export default function AntelopeHarvest() {
   return (
-    <div className="container mx-auto pt-2 pb-4 h-[calc(100vh-8rem)]">
+    <div className="container mx-auto pt-2 pb-10 h-[calc(100vh-8rem)]">
       <div className="flex items-start gap-6 mb-2">
         <div className="flex-shrink-0">
           <h1 className="text-3xl font-bold mb-1">Antelope Harvest Statistics</h1>
