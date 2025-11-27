1cda5e0c4e256259b4095902835b1ef442ef54b5
carrellcody <carrellcody18@gmail.com>
2025-11-26 20:15:32 -0600

    Adjust padding in ElkDraw component
    Updated padding in the ElkDraw component.

--- src/pages/ElkDraw.tsx
+++ src/pages/ElkDraw.tsx
@@ -21,7 +21,7 @@ export default function ElkDraw() {
   }
 
   return (
-    <div className="container mx-auto pt-2 pb-4 h-[calc(100vh-8rem)]">
+    <div className="container mx-auto pt-2 pb-10 h-[calc(100vh-8rem)]">
       <div className="flex items-start gap-6 mb-2">
         <div className="flex-shrink-0">
           <h1 className="text-3xl font-bold mb-1">Elk Draw Statistics</h1>
